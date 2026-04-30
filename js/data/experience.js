d05b1f1d73ab56eff6e2f01f83a2546a0063bda6
incharadn433 <incharadn433@gmail.com>
2026-04-30 09:47:23 +0530

    Portfolio project

---
+++ js/data/experience.js
@@ -0,0 +1,25 @@
+const experiencesData = [
+     {
+    id: 1,
+    label: "VTU",
+    college:"CIT Mandya",
+    study:"B.E in computer science(2022-2026)",
+    CGPA:"CGPA:8.67"
+  },
+  {
+    id: 2,
+    label: "PUC",
+    college:"Bharathi College",
+    study:"Science:PCMB(2021-2022)",
+    CGPA:"SCORE:78%"
+  },
+   {
+    id: 3,
+    label: "KSEAB",
+    college:"Government High school,Chikkankanahalli",
+    study:"SSLC(2019-2020)",
+    CGPA:"SCORE:96.64%"
+  }
+
+    
+];
---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    //Render skills
+    renderSkills();
+    initModal();
+    initContactValidation();
+    initThemeToggle();
+    ExperienceView();
+    initTypingAnimation();
+    initSearch();
+    initGeolocation();
+    initDynamicGreeting();
+});
